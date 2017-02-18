ce30271f8397af7cce4a687338b30e6bbe01f196
James Hill <j.g.hill@zoho.com>
2017-02-18 20:51:14 +0000

    Added code for describing objects at a place.

--- Pangu.pl
+++ Pangu.pl
@@ -1,23 +1,26 @@
-/* Pangu: The Myth of Creation, by James Hill. */
+/* Pangu: The Myth of Creation, by James Hill.
+Dynamic variables. */
 
-/* The instructions for the game */
+i_am_at(chaos).
+
+/* Instructions for the game. */
 
 instructions :-
 	nl,
-	write('Enter commands using standard Prolog syntax'), nl,
-	write('Available commands are:'), nl,
-	write(' start.			-- to start the game.'), nl,
-	write('	n.	s.	e.	w.	-- to go in that direction.'), nl,
-	write('	take(Object).	-- to pick up an object.'), nl,
-	write('	drop(Object).	-- to drop an object.'), nl,
-	write('	swing(Object).	-- to swing an object.'), nl,
-	write('	lead(Object).	-- to lead an object.'), nl,
-	write('	look.			-- to look around you again.'), nl,
-	write('	instructions.	-- to see this message again.'), nl,
-	write('	halt.			-- to end the game and quit.'), nl,
+	write('Enter commands using standard Prolog syntax. Available commands are:'), nl, nl,
+	write('	start.					-- to start the game.'), nl,
+	write('	n.	s.	e.	w.		-- to go in that direction.'), nl,
+	write('	take(Object).				-- to pick up an object.'), nl,
+	write('	drop(Object).				-- to drop an object.'), nl,
+	write('	swing(Object).				-- to swing an object.'), nl,
+	write('	lead(Object).				-- to lead an object.'), nl,
+	write('	look.					-- to look around you again.'), nl,
+	write('	examine(Object).			-- to examine something in more detail.'), nl,
+	write('	instructions.				-- to see this message again.'), nl,
+	write('	halt.					-- to end the game and quit.'), nl,
 	nl.
 
-/* Start the game, show instructions, and  */
+/* Start the game, show instructions, and describe immediate environment. */
 
 start :-
 	instructions,
@@ -26,17 +29,17 @@ start :-
 /* Explore the environment */
 
 look :-
-	describe(Place),
-	nl,
-	notice_objects_at(Place),
-	nl.
+	i_am_at(Place),
+	describe(Place), nl,
+	notice_objects_at(Place), nl.
 
 describe(chaos) :-
 	write('You stand in the midst of swirling chaos.'), nl,
 	write('Yin spirals and twirls around Yang.'), nl,
-	write('Yang spirals and twirls around Yin.'), nl,
-	write('Beside you is your broken Cosmic Egg.'), nl,
-	write('The broken Cosmic Egg has the hole from you climbed on the top.'), nl.
+	write('Yang spirals and twirls around Yin.'), nl.
+
+/*	write('Beside you is your broken Cosmic Egg.'), nl,
+	write('The broken Cosmic Egg has the hole from which you climbed on top.'), nl. */
 
 describe(worldCenter) :-
 	write('You stand between the Yin and Yang.'), nl,
@@ -44,9 +47,29 @@ describe(worldCenter) :-
 	write('Clear Yang forms a sky above your head.'), nl,
 	write('But the Yin and Yang are starting to mix again.'), nl.
 
-/* These rules describe the objects in the environment */
+describe(worldNorth) :-
+	describe(worldCenter).
+
+describe(worldSouth) :-
+	describe(worldCenter).
+
+describe(worldEast) :-
+	describe(worldCenter).
+
+describe(worldWest) :-
+	describe(worldCenter).
+
+/* Describe the objects in the environment */
 
 notice_objects_at(Place) :-
 	at(X, Place),
-	write('There is a '), write(X), write(' here.') nl,
+	write('Neraby is a '), write(X), write('.'), nl,
 	fail.
+
+notice_objects_at(_).
+
+at('cosmic Egg with a hole on the top', chaos).
+at('giant Turtle with a beard', worldNorth).
+at('scaly Qilin with hooves', worldSouth).
+at('multi-coloured Phoenix with the tail of a fish', worldEast).
+at('coiling dragon with long nostril hairs', worldWest).
